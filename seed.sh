caa5b9d1e89e52f2bc27bc1d05b69607c79ab90d
Zebreus <zebreus@zebre.us>
2026-05-10 18:42:02 +0200

    Rename upstream dir

--- seed.sh
+++ seed.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-svnadmin create "$(pwd)/svn-mirror"
+svnadmin create "$(pwd)/upstream"
 echo '#!/bin/sh' > hooks/pre-revprop-change
 chmod +x hooks/pre-revprop-change
-svnsync init "file://$(pwd)/svn-mirror" svn://svn.code.sf.net/p/tom7misc/svn/trunk
-svnsync sync "file://$(pwd)/svn-mirror"
+svnsync init "file://$(pwd)/upstream" svn://svn.code.sf.net/p/tom7misc/svn/trunk
+svnsync sync "file://$(pwd)/upstream"
